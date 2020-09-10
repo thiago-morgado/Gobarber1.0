d285ab00200e1390821d5988945401134ff6ee7d
thiago morgado <thiagomorgado81@gmail.com>
2020-09-10 03:20:10 -0300

    implementado multer com jwt

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
@@ -7,12 +9,16 @@ import SessionController from './app/controller/SessionController';
 import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 // Rota global
 routes.use(authMiddlewares);
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json(req.file);
+});
 routes.put('/users', UserController.update);
 
 export default routes;
